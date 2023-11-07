8cf4196ea5f852d57c5cce714b479548f0c305c9
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-07 01:27:25 -0400

    Test my first states structure

--- react-states/src/App.jsx
+++ react-states/src/App.jsx
@@ -1,4 +1,24 @@
 import './App.css'
+import React from 'react'
 
+class App extends React.Component { 
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: 'Alberto',
+      city: 'Bogotá'
+    }
+  }
+
+  render() { 
+    return (
+      <div>
+        <h1>React States</h1>
+        <p>My name is {this.state.name}</p>
+        <p>I live in {this.state.city}</p>
+      </div>
+    )
+  }
+}
 
 export default App
