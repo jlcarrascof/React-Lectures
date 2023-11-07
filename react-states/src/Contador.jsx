867b091acc0328ec50b583963fbc82fa8568a588
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-07 14:05:39 -0400

    Refactoring the code

--- react-states/src/Contador.jsx
+++ react-states/src/Contador.jsx
@@ -3,10 +3,14 @@ import { useState } from 'react';
 function Contador() { 
    const [counter, setCounter] = useState(0);
    
+   function handleClick() {
+       setCounter(counter + 1);
+   }
+
    return ( 
     <div>
         <h3>Contador: {counter}</h3>
-        <button onClick={() => setCounter(counter + 1)}>Suma uno!</button>
+        <button onClick={handleClick}>Suma uno!</button>
     </div>
    );     
 }
