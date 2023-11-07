d694ce185599c7cda60bef268b4475831de29d06
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-07 13:50:02 -0400

    Create handleClick function to change the state

--- react-states/src/App.jsx
+++ react-states/src/App.jsx
@@ -5,7 +5,6 @@ function App() {
   return <div>
     <Contador />
   </div>
-
 }
 
 export default App
--- react-states/src/Contador.jsx
+++ react-states/src/Contador.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 function Contador() { 
-    const [counter, setCounter] = React.useState(0);
+   const [counter, setCounter] = React.useState(0);
+   
+   function handleClick() {
+       setCounter(counter + 1);
+   }
+
+   return ( 
+    <div>
+        <h3>Contador: {counter}</h3>
+        <button onClick={handleClick}>Suma uno!</button>
+    </div>
+   );     
 }
 
 export default Contador;
