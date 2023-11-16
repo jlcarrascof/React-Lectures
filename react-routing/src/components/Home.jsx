4f4ecaf6dad2ca41d95f83b56984a01f09756edf
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-16 19:28:48 -0400

    Practicing the hook useNavigate

--- react-routing/src/components/Home.jsx
+++ react-routing/src/components/Home.jsx
@@ -4,11 +4,11 @@ import { useNavigate } from "react-router-dom";
 
 function Home() {
   const navigate = useNavigate()
-  // console.log(navigate);
+  console.log(navigate);
   return (
     <div>
       <h1>Bienvenidos</h1>
-      <NavLink to='/about'><h6>About</h6></NavLink> 
+      <Link to='/about'><h6>About</h6></Link> 
       <NavLink to='/gallery'><h6>Gallery</h6></NavLink>
       <NavLink to='/contact'><h6>Contact</h6></NavLink>
     </div>
