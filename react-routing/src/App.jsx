545bd2d3a02d138c23a5bf52639c691cbc6eeb72
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-15 21:47:18 -0400

    Import the main components in our lecture

--- react-routing/src/App.jsx
+++ react-routing/src/App.jsx
@@ -1,5 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import {Routes, Route} from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Gallery from './components/Gallery'
+
 
 function App() {
 
