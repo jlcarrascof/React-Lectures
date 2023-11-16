ac44f671819daa0dea7918e7d7252226e49c9060
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-16 12:19:21 -0400

    Finish the NavLink component to be reusable

--- react-routing/src/components/Navlink.jsx
+++ react-routing/src/components/Navlink.jsx
@@ -4,33 +4,16 @@ function Navlink({ to, children, ...props }) {
   return <div>
     <NavLinkComp {...props}
     to={to}
-    
+    style={({ isActive })=> { 
+      return {
+        fontWeight: isActive ? 'bold' : '',
+        color: isActive ? 'red' : 'black'
+      }
+    }}
     >
-
+      {children}
     </NavLinkComp>  
   </div>
 }
 
 export default Navlink;
-
-/* Codigo original
-import { NavLink as NavLinkComp} from "react-router-dom"
-
-function NavLink({ to, children, ...props }) {
-  return (
-    <NavLinkComp 
-    {...props}
-    to={to}
-    style={({ isActive })=> {
-      return {
-        fontWeight: isActive ? 'bold' : '',
-        color: isActive ? 'red' : 'black'
-      }
-    } }
-    >
-      {children}
-    </NavLinkComp>
-  )
-}
-export default NavLink;
-*/
