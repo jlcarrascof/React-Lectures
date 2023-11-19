6ef78a583d7cda7e26de1c1b4ffa531a36fbab88
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-19 01:51:01 -0400

    Create the fields alumno and email with the events

--- react-forms/src/App.jsx
+++ react-forms/src/App.jsx
@@ -10,13 +10,17 @@ function App() {
     setAlumno(event.target.value)
   }
 
+  const handleChangeEmail = (event) => { 
+    setEmail(event.target.value)
+  }
+
   return <div>
     <form>
       <label htmlFor="alumno">Alumno: </label>
       <input type="text" id="alumno" name="alumno" value={alumno} onChange={handleChangeAlumno}
       placeholder="Ingrese el nombre del Alumno" />
       <label htmlFor="email">Correo: </label>
-      <input type="email" id="email" name="email" value={email} onChange={(event) => setEmail(event.target.value)}
+      <input type="email" id="email" name="email" value={email} onChange={handleChangeEmail}
       placeholder="Ingrese el Correo Electrónico" />
     </form>  
   </div>
