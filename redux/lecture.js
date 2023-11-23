e2d700604f969123fa0e44dd0d7adedb1ee0c50d
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-23 01:46:43 -0400

    Create global state and reducer

--- redux/lecture.js
+++ redux/lecture.js
@@ -7,5 +7,29 @@ const redux = require('redux');
 const createStore = redux.createStore;
 
 // 3.- Tipos de acción.
+
 const ADD_TODO = 'ADD_TODO';
 const REMOVE_TODO = 'REMOVE_TODO';
+
+// 4.- Estructura del estado global y reducer.
+
+const initialState = {
+    todos: []
+}
+
+function reducer(state = initialState, action){
+    switch(action.type){
+        case ADD_TODO:
+            return {
+                todos:[...state.todos, action.payload]
+            }
+        case REMOVE_TODO:
+            return {
+                todos: state.todos.filter((text, i) => i !== action.payload)
+            }
+        default:
+            return state;
+    }
+}
+
+// 5.- Crear store.
