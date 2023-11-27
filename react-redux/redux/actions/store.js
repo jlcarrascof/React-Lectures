729b4e8e25bc4bc0d0abd6c87e1dd44d056fa1ba
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-26 22:45:54 -0400

    Move files to redux folder

--- react-redux/redux/actions/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore, compose } from "redux";
-import reducer from "./reducer";
-
-const composeDevTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(reducer, composeDevTools());
-
-
