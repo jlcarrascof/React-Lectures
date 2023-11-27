9758bc3a44b84f6913394f7e9625b53987bf5e1f
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-27 01:25:56 -0400

    Coding our reducer

--- react-redux/redux/reducer.js
+++ react-redux/redux/reducer.js
@@ -1,5 +1,26 @@
-function reducer() {
+import { INCREMENT, DECREMENT, RESET, GET_POST, RECEIVE_POST } from './actionTypes';
 
+const initialState = {
+  count: 0,
+  loading: false,
+  post: {},
+}
+
+function reducer(state = initialState, { type, payload }) {
+    switch (type) {
+      case INCREMENT:
+        return { ...state, count: state.count + 1 };
+      case DECREMENT:
+        return { ...state, count: state.count - 1 };
+      case RESET:
+        return { ...state, count: 0 };
+      case GET_POST:
+        return { ...state, loading: true };
+      case RECEIVE_POST:
+        return { ...state, loading: false, post: payload };
+      default:
+        return state;
+    }
 }
 
 export default reducer;
