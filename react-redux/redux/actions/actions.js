ab1b9f860150c54e9560cd99484921f25865990b
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-27 01:18:02 -0400

    Coding fetchPost function

--- react-redux/redux/actions/actions.js
+++ react-redux/redux/actions/actions.js
@@ -1,4 +1,5 @@
 import { INCREMENT, DECREMENT, RESET, GET_POST, RECEIVE_POST } from "./actionsTypes";
+import axios from 'axios';
 
 export function increment() { 
   return { type: INCREMENT };
@@ -16,15 +17,17 @@ export function getPost() {
   return { type: GET_POST };
 }
 
-export function receivePost(dato) { 
+export function receivePost(dato) { //* accion sincrona
   return { type: RECEIVE_POST, payload: dato };
 } 
 
-export function fetchPost() {
-  return dispatch => {
+export function fetchPost(valor) { //* accion asincrona
+  return function(dispatch) {
     dispatch(getPost());
-    return fetch('https://jsonplaceholder.typicode.com/posts/1')
-      .then(response => response.json())
-      .then(json => dispatch(receivePost(json)))
-  }
+    axios(`https://jsonplaceholder.typicode.com/todos/${valor}`)
+      .then((response) => {
+        console.log(response)
+        return dispatch(receivePost(response.data))
+      })
+  } 
 }
