51a0fc9cda90f41189e66be50d56c96367cb3424
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-27 22:45:32 -0400

    First version of Counter

--- react-redux/src/components/Counter.jsx
+++ react-redux/src/components/Counter.jsx
@@ -1,7 +1,11 @@
-
-
 function Counter() {
-
+    return <div>
+        <span>Contador:</span>
+        <button>Incrementar</button>
+        <button>Decrementar</button>
+        <button>Resetear</button>
+        <button>Enviar Número a API</button>        
+    </div>      
 }
 
 export default Counter;
