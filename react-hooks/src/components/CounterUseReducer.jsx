49b1f2c4aa39fd1cc7a5c455bba5b02e9c2c1097
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-12-03 02:27:23 -0400

    Create the component CounterUseReducer

---
+++ react-hooks/src/components/CounterUseReducer.jsx
@@ -0,0 +1,37 @@
+import { useSelector, useDispatch } from 'react-redux';
+import * as actionsCreators from '../redux/actions/actions';  //* Importa todas las acciones
+
+function Counter() {
+
+    const counter = useSelector(state => state.count); //* useSelector() es un hook que permite acceder al estado de redux
+    const dispatch = useDispatch(); //* useDispatch() es un hook que permite acceder a dispatch()
+
+    function increment() { 
+        dispatch(actionsCreators.increment()) 
+    }
+
+    function decrement() {
+        dispatch(actionsCreators.decrement()) 
+    }
+    
+    function reset() {
+        dispatch(actionsCreators.reset()) 
+    }
+
+    function fetchPost(counter) {
+        dispatch(actionsCreators.fetchPost(counter)) 
+    }
+
+    return <div>
+        <span>Contador: {counter}</span>
+        <br />
+        <div>
+            <button onClick={increment}>Incrementar</button>
+            <button onClick={decrement}>Decrementar</button>
+            <button onClick={reset}>Resetear</button>
+            <button onClick={() => fetchPost(counter)}>Enviar Número a API</button>
+        </div>            
+    </div>      
+}
+
+export default Counter;
