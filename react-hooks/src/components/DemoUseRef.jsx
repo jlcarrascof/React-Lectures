ce8e8362e58fbec6a43ac1c0aef1e1941acfc475
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-12-05 01:30:08 -0400

    Finish coding DemoUseRef component

--- react-hooks/src/components/DemoUseRef.jsx
+++ react-hooks/src/components/DemoUseRef.jsx
@@ -1,7 +1,16 @@
 import { useRef } from 'react';
 
 function DemoUseRef() {
+    const inputElem = useRef(null);
 
+    function handleClick() {
+        inputElem.current.focus();
+    }
+
+    return <div>
+        <input type="text" ref={inputElem} />
+        <button onClick={handleClick}>Focus en el Input</button>
+    </div>
 }
 
 export default DemoUseRef;
