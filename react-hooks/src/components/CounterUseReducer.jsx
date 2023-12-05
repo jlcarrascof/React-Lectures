fc7cfab4b4ab8aa7eb12799cf2c1a19c45a94ddb
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-12-04 22:47:54 -0400

    Finish CounterUseReducer component

--- react-hooks/src/components/CounterUseReducer.jsx
+++ react-hooks/src/components/CounterUseReducer.jsx
@@ -1,35 +1,62 @@
 import { useSelector, useDispatch } from 'react-redux';
+import { useReducer } from 'react';
 import * as actionsCreators from '../redux/actions/actions';  //* Importa todas las acciones
+import { INCREMENT, DECREMENT, RESET, GET_POST, RECEIVE_POST } from './actions/actionsTypes';
+
+const initialState = {
+    count: 0,
+    loading: false,
+    post: {},
+}
+  
+function reducerComponente(state = initialState, { type, payload }) {
+    switch (type) {
+      case INCREMENT:
+        return { ...state, count: state.count + 1 };
+      case DECREMENT:
+        return { ...state, count: state.count - 1 };
+      case RESET:
+        return { ...state, count: 0 };
+      case GET_POST:
+        return { ...state, loading: true };
+      case RECEIVE_POST:
+        return { ...state, loading: false, post: payload };
+      default:
+        return { ...state };
+    }
+}
 
 function CounterUseReducer() {
 
     const counter = useSelector(state => state.count); //* useSelector() es un hook que permite acceder al estado de redux
     const dispatch = useDispatch(); //* useDispatch() es un hook que permite acceder a dispatch()
 
+    const [counterComp, dispatchComp] = useReducer(reducerComponente, initialState);
+
     function increment() { 
-        dispatch(actionsCreators.increment()) 
+        dispatchComp(actionsCreators.increment()) 
     }
 
     function decrement() {
-        dispatch(actionsCreators.decrement()) 
+        dispatchComp(actionsCreators.decrement()) 
     }
     
     function reset() {
-        dispatch(actionsCreators.reset()) 
+        dispatchComp(actionsCreators.reset()) 
     }
 
     function fetchPost(counter) {
-        dispatch(actionsCreators.fetchPost(counter)) 
+        dispatchComp(actionsCreators.fetchPost(counter)) 
     }
 
     return <div>
-        <span>Contador: {counter}</span>
+        <span>Contador use Reducer: {counterComp}</span>
         <br />
         <div>
             <button onClick={increment}>Incrementar</button>
             <button onClick={decrement}>Decrementar</button>
             <button onClick={reset}>Resetear</button>
-            <button onClick={() => fetchPost(counter)}>Enviar Número a API</button>
+            <button onClick={() => fetchPost(counterComp)}>Enviar Número a API</button>
         </div>            
     </div>      
 }
